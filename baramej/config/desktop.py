3ce80fd1c3ce2b6762a1f1128c361aac9e6769ea
Yazansammour <yazansammour@gmail.com>
2021-05-08 04:43:13 +0400

    Update desktop.py

--- baramej/config/desktop.py
+++ baramej/config/desktop.py
@@ -5,7 +5,7 @@ from frappe import _
 def get_data():
 	return [
 		{
-			"module_name": "Baramej",
+			"module_name": "Baramej app",
 			"color": "grey",
 			"icon": "octicon octicon-file-directory",
 			"type": "module",
